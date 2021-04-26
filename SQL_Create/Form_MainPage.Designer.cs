fba6fe5979e04a9818516f68d27ffdc8e41211b2
Peter11300 <65581934+Peter11300@users.noreply.github.com>
2021-04-26 10:43:36 +0800

    Initialization

---
+++ SQL_Create/Form_MainPage.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace SQLCommandString {
+    partial class Form_MainPage {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent() {
+            this.button_Create = new System.Windows.Forms.Button();
+            this.SQL_CommandString = new System.Windows.Forms.TextBox();
+            this.button_Alter = new System.Windows.Forms.Button();
+            this.DegreeOfCompletion = new System.Windows.Forms.Label();
+            this.button_Cooy = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button_Create
+            // 
+            this.button_Create.BackColor = System.Drawing.Color.PapayaWhip;
+            this.button_Create.FlatAppearance.BorderSize = 0;
+            this.button_Create.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_Create.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button_Create.Location = new System.Drawing.Point(9, 13);
+            this.button_Create.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button_Create.Name = "button_Create";
+            this.button_Create.Size = new System.Drawing.Size(112, 43);
+            this.button_Create.TabIndex = 3;
+            this.button_Create.Text = "新增資料表";
+            this.button_Create.UseVisualStyleBackColor = false;
+            this.button_Create.Click += new System.EventHandler(this.button_CreateTable_Click);
+            // 
+            // SQL_CommandString
+            // 
+            this.SQL_CommandString.HideSelection = false;
+            this.SQL_CommandString.Location = new System.Drawing.Point(127, 13);
+            this.SQL_CommandString.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.SQL_CommandString.Multiline = true;
+            this.SQL_CommandString.Name = "SQL_CommandString";
+            this.SQL_CommandString.ReadOnly = true;
+            this.SQL_CommandString.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.SQL_CommandString.Size = new System.Drawing.Size(459, 405);
+            this.SQL_CommandString.TabIndex = 4;
+            // 
+            // button_Alter
+            // 
+            this.button_Alter.BackColor = System.Drawing.Color.PapayaWhip;
+            this.button_Alter.FlatAppearance.BorderSize = 0;
+            this.button_Alter.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_Alter.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button_Alter.Location = new System.Drawing.Point(9, 69);
+            this.button_Alter.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button_Alter.Name = "button_Alter";
+            this.button_Alter.Size = new System.Drawing.Size(112, 43);
+            this.button_Alter.TabIndex = 5;
+            this.button_Alter.Text = "增修欄位";
+            this.button_Alter.UseVisualStyleBackColor = false;
+            this.button_Alter.Click += new System.EventHandler(this.button_Alter_Click);
+            // 
+            // DegreeOfCompletion
+            // 
+            this.DegreeOfCompletion.AutoSize = true;
+            this.DegreeOfCompletion.Font = new System.Drawing.Font("微軟正黑體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.DegreeOfCompletion.Location = new System.Drawing.Point(140, 438);
+            this.DegreeOfCompletion.Name = "DegreeOfCompletion";
+            this.DegreeOfCompletion.Size = new System.Drawing.Size(129, 27);
+            this.DegreeOfCompletion.TabIndex = 6;
+            this.DegreeOfCompletion.Text = "完成率：0％";
+            // 
+            // button_Cooy
+            // 
+            this.button_Cooy.BackColor = System.Drawing.Color.SkyBlue;
+            this.button_Cooy.FlatAppearance.BorderSize = 0;
+            this.button_Cooy.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_Cooy.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button_Cooy.Location = new System.Drawing.Point(450, 426);
+            this.button_Cooy.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button_Cooy.Name = "button_Cooy";
+            this.button_Cooy.Size = new System.Drawing.Size(112, 43);
+            this.button_Cooy.TabIndex = 7;
+            this.button_Cooy.Text = "複製內容";
+            this.button_Cooy.UseVisualStyleBackColor = false;
+            this.button_Cooy.Click += new System.EventHandler(this.button_Cooy_Click);
+            // 
+            // Form_MainPage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(598, 481);
+            this.Controls.Add(this.button_Cooy);
+            this.Controls.Add(this.DegreeOfCompletion);
+            this.Controls.Add(this.button_Alter);
+            this.Controls.Add(this.SQL_CommandString);
+            this.Controls.Add(this.button_Create);
+            this.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form_MainPage";
+            this.Text = "SQL字串產生";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button_Create;
+        private System.Windows.Forms.TextBox SQL_CommandString;
+        private System.Windows.Forms.Button button_Alter;
+        private System.Windows.Forms.Label DegreeOfCompletion;
+        private System.Windows.Forms.Button button_Cooy;
+    }
+}
+
---
+++ SQL_Create/Form_MainPage.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SQLCommandString {
+    public partial class Form_MainPage : Form {
+        public Form_MainPage() {
+            InitializeComponent();
+        }
+
+        public DataTable ImportExcel() {
+            string windowFilter = "Excel files|*.xlsx";
+            string windowTitle = "匯入Excel資料";
+
+            OpenFileDialog openFileDialogFunction = new OpenFileDialog();
+            openFileDialogFunction.Filter = windowFilter; //開窗搜尋副檔名
+            openFileDialogFunction.Title = windowTitle; //開窗標題
+
+            DataTable dataTable = new DataTable();
+
+            if (openFileDialogFunction.ShowDialog() == DialogResult.OK) {
+                //定義OleDb======================================================
+                //1.檔案位置
+                string FilePath = openFileDialogFunction.FileName;
+
+                //2.提供者名稱  Microsoft.Jet.OLEDB.4.0適用於2003以前版本，Microsoft.ACE.OLEDB.12.0 適用於2007以後的版本處理 xlsx 檔案
+                string ProviderName = "Microsoft.ACE.OLEDB.12.0;";
+
+                //3.Excel版本，Excel 8.0 針對Excel2000及以上版本，Excel5.0 針對Excel97。
+                string ExtendedString = "'Excel 8.0;";
+
+                //4.第一行是否為標題(;結尾區隔)
+                string HDR = "No;";
+
+                //5.IMEX=1 通知驅動程序始終將「互混」數據列作為文本讀取(;結尾區隔,'文字結尾)
+                string IMEX = "0';";
+
+                //=============================================================
+                //連線字串
+                string connectString =
+                        "Data Source=" + FilePath + ";" +
+                        "Provider=" + ProviderName +
+                        "Extended Properties=" + ExtendedString +
+                        "HDR=" + HDR +
+                        "IMEX=" + IMEX;
+                //=============================================================
+
+
+                using (OleDbConnection Connect = new OleDbConnection(connectString)) {
+                    try {
+                        Connect.Open();
+                    } catch (Exception ex) {
+                        MessageBox.Show("請關閉目前開啟的Excel檔案");
+                        return dataTable;
+                    }
+                    //=============================================================
+                    DataTable dataTable_sheetname = Connect.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
+
+                    foreach (DataRow row in dataTable_sheetname.Rows) {
+                        // Write the sheet name to the screen
+
+                        //就是在這取得Sheet Name
+                        //=============================================================
+
+                        string queryString = "SELECT * FROM [" + row["TABLE_NAME"].ToString() + "]";
+                        try {
+                            using (OleDbDataAdapter dr = new OleDbDataAdapter(queryString, Connect)) {
+                                dr.Fill(dataTable);
+                            }
+                        } catch (Exception ex) {
+                            MessageBox.Show("異常訊息:" + ex.Message, "異常訊息");
+                        }
+                    }
+
+
+                }
+            }
+
+            return dataTable;
+        }
+
+        private String getStartEndLocation(DataTable ExcelTable) {
+            String location = "s0";
+            for (int i = 0; i < ExcelTable.Rows.Count; i++) {
+                if (i + 1 < ExcelTable.Rows.Count) {
+                    if (!(ExcelTable.Rows[i]["規格書"].ToString().Equals(ExcelTable.Rows[i + 1]["規格書"].ToString()))) {
+                        location += ",e" + i + ",s" + (i + 1);
+                    }
+                } else {
+                    location += ",e" + i;
+                }
+
+            }
+            return location;
+        }
+
+        private String getPrimaryKeyLocation(DataTable ExcelTable) {
+            String location = "";
+            for (int i = 0; i < ExcelTable.Rows.Count; i++) {
+                if (ExcelTable.Rows[i]["KEY"].ToString() == "P") {
+                    location += "p" + i + ",";
+                }
+            }
+            return location;
+        }
+
+        private String getcolumnString(DataTable ExcelTable, int ExcelTableIndex) {
+            String columnString = "";
+            columnString = ExcelTable.Rows[ExcelTableIndex]["資料行名稱"].ToString() + " " + ExcelTable.Rows[ExcelTableIndex]["資料類型"].ToString() + " " + ExcelTable.Rows[ExcelTableIndex]["允許Null"].ToString() + ", \r\n";
+
+            return columnString;
+        }
+
+        private String getCreateString(DataTable ExcelTable, String _startEndLocation, String _primaryKeyLocation) {
+            String[] startEndLocation = _startEndLocation.Split(',');
+            String[] primaryKeyLocation = _primaryKeyLocation.Split(',');
+            int startEndLocationIndex = 0;
+            int primaryKeyLocationIndex = 0;
+            String commandString = "";
+            String temp_commandString = "";
+            String primaryString = "\r\nPRIMARY KEY(";
+            Boolean primaryCheck = true;
+
+            for (int i = 0; i < ExcelTable.Rows.Count; i++) {
+                setDegreeOfCompletionText(i, ExcelTable.Rows.Count);
+                if (ExcelTable.Rows[i]["修改註記V"].ToString() == "AT") {
+                    if ("p" + i == primaryKeyLocation[primaryKeyLocationIndex]) {
+                        if (primaryCheck == false) {
+                            primaryString = primaryString + "," + ExcelTable.Rows[i]["資料行名稱"].ToString();
+                            primaryKeyLocationIndex++;
+                        } else {
+                            primaryString = primaryString + ExcelTable.Rows[i]["資料行名稱"].ToString();
+                            primaryCheck = false;
+                            primaryKeyLocationIndex++;
+                        }
+                    }
+
+                    if ("e" + i == startEndLocation[startEndLocationIndex]) {
+
+                        temp_commandString += getcolumnString(ExcelTable, i);
+
+                        temp_commandString = temp_commandString + primaryString + ") \r\n";
+                        temp_commandString += "); \r\n\r\n";
+
+                        commandString += temp_commandString;
+                        primaryString = "\r\nPRIMARY KEY(";
+                        temp_commandString = "";
+                        startEndLocationIndex++;
+                        primaryCheck = true;
+                    } else {
+                        if ("s" + i == startEndLocation[startEndLocationIndex]) {
+                            String createTableString = "CREATE TABLE ";
+                            createTableString = createTableString + ExcelTable.Rows[i]["規格書"].ToString() + "\r\n( \r\n";
+                            temp_commandString += createTableString;
+                            startEndLocationIndex++;
+                        }
+
+                        temp_commandString += getcolumnString(ExcelTable, i);
+                    }
+                }
+
+            }
+
+            return commandString;
+        }
+
+        private String getAlterString(DataTable ExcelTable) {
+            String commandString = "";
+            String commandAddString = "";
+            String commandDropString = "";
+            String commandModifyString = "";
+            String storedprocedureString = "";
+
+            for (int i = 0; i < ExcelTable.Rows.Count; i++) {
+                setDegreeOfCompletionText(i, ExcelTable.Rows.Count);
+                switch (ExcelTable.Rows[i]["修改註記V"].ToString()) {
+                    case "A":
+                        commandAddString = commandAddString + getAlterAddString(ExcelTable.Rows[i]);
+                        break;
+                    case "D":
+                        commandDropString = commandDropString + getAlterDropString(ExcelTable.Rows[i]);
+                        break;
+                    case "C":
+                        storedprocedureString = storedprocedureString + getAlterChangeString(ExcelTable.Rows[i]);
+                        break;
+                    case "M":
+                        commandModifyString = commandModifyString + getAlterModifyString(ExcelTable.Rows[i]);
+                        break;
+
+                }
+            }
+
+            commandString = commandString + commandAddString + commandDropString + commandModifyString;
+
+            if (storedprocedureString != "") {
+                commandString = commandString + "\r\n==========================\r\n" + "======== 以下請逐行執行 ========\r\n" + "==========================\r\n";
+                commandString = commandString + storedprocedureString;
+            }
+
+            return commandString;
+        }
+
+        private String getAlterAddString(DataRow dataRow) {
+            String commandString = "";
+
+            commandString = commandString + "ALTER TABLE " + dataRow["規格書"].ToString() + " ADD " + dataRow["資料行名稱"].ToString() + " " + dataRow[4].ToString() + " ;\r\n";
+
+            return commandString;
+        }
+
+        private String getAlterDropString(DataRow dataRow) {
+            String commandString = "";
+
+            commandString = commandString + "ALTER TABLE " + dataRow["規格書"].ToString() + " DROP COLUMN " + dataRow["資料行名稱"].ToString() + " ;\r\n";
+
+            return commandString;
+        }
+
+        private String getAlterChangeString(DataRow dataRow) {
+            String commandString = "";
+
+            commandString = commandString + "\r\n sp_rename '" + dataRow["規格書"].ToString() + "." + dataRow["原欄位名稱"].ToString() + "', '" + dataRow["資料行名稱"].ToString() + "', '" + "COLUMN' ;\r\n";
+
+            return commandString;
+        }
+
+        private String getAlterModifyString(DataRow dataRow) {
+            String commandString = "";
+
+            commandString = commandString + "ALTER TABLE " + dataRow["規格書"].ToString() + " ALTER COLUMN " + dataRow["資料行名稱"].ToString() + " " + dataRow["資料類型"].ToString() + " ;\r\n";
+
+            return commandString;
+        }
+
+        private void setSQL_CommandStringText(String CommandString) {
+
+            Clipboard.SetData(DataFormats.Text, CommandString);
+
+            SQL_CommandString.Text = CommandString;
+        }
+
+        private void button_CreateTable_Click(object sender, EventArgs e) {
+            DataTable TableValue = ImportExcel();
+            String startEndLocation = getStartEndLocation(TableValue);
+            String primaryKeyLocation = getPrimaryKeyLocation(TableValue);
+            String commandString = getCreateString(TableValue, startEndLocation, primaryKeyLocation);
+
+            setSQL_CommandStringText(commandString);
+        }
+
+        private void button_Alter_Click(object sender, EventArgs e) {
+            DataTable TableValue = ImportExcel();
+            String commandString = getAlterString(TableValue);
+
+            setSQL_CommandStringText(commandString);
+        }
+
+        private void setDegreeOfCompletionText(int current, int total) {
+            if ((total - 1) != 0) {
+                DegreeOfCompletion.Text = "完成率：" + (current * 100 / (total - 1)) + "％";
+            }
+        }
+
+        private void button_Cooy_Click(object sender, EventArgs e) {
+
+            SQL_CommandString.SelectAll();
+            SQL_CommandString.Copy();
+        }
+    }
+}
+
