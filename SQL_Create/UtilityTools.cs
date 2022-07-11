70bbe64d2fbd11e1e0a9f64da6ab1d27e5bdb914
Peter11300 <65581934+Peter11300@users.noreply.github.com>
2022-07-11 17:24:28 +0800

    Add utility class

--- SQL_Create/UtilityTools.cs
+++ SQL_Create/UtilityTools.cs
@@ -1,12 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Data;
 using System.Data.OleDb;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using Excel = Microsoft.Office.Interop.Excel;
 
