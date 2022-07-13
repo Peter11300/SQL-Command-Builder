9dc6d900e87536b10b7abb15d2a5599e0a996640
Peter11300 <65581934+Peter11300@users.noreply.github.com>
2022-07-13 11:34:15 +0800

    Rename solution

--- SQL_Create/Properties/Resources.Designer.cs
+++ SQL_Create/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace SQLCommandString.Properties {
+namespace SqlCommandBuilder.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace SQLCommandString.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("SQLCommandString.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("SqlCommandBuilder.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
--- SQL_Create/Properties/Settings.Designer.cs
+++ SQL_Create/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace SQLCommandString.Properties {
+namespace SqlCommandBuilder.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
